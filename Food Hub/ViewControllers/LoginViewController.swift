f4e227af43196350dcb25cec889154f81d5aca43
SaGe <m.ozturkes@gmail.com>
2023-12-06 20:43:50 +0300

    done!

--- Food Hub/ViewControllers/LoginViewController.swift
+++ Food Hub/ViewControllers/LoginViewController.swift
@@ -7,25 +7,63 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var googleButtton: UIButton!
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var LoginButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    
+    let customColor = UIColor(hex: 0xFF7252)
+    var selectedTextField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
       skipButtonUI()
+        passwordTextfield.delegate = self
+        emailTextfield.delegate = self
+       
+        
+       
+        emailTextfield.tag = 0
+        passwordTextfield.tag = 1
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+                view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        // Boş bir yere tıklandığında klavyeyi kapat
+        view.endEditing(true)
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Seçilen TextField'in sınır rengini değiştir
+        selectedTextField = textField
+        selectedTextField?.layer.borderColor = customColor.cgColor
+        selectedTextField?.layer.borderWidth = 1.0
+    }
 
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Seçilen TextField'in sınır rengini eski haline getir
+        selectedTextField?.layer.borderColor = nil
+        selectedTextField?.layer.borderWidth = 0.0
+        selectedTextField = nil
+    }
+    
+    @IBAction func SignUpButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        }
+    
     func skipButtonUI() {
         
         backButton?.layer.shadowColor = UIColor.lightGray.cgColor
         backButton?.layer.shadowOffset = CGSize(width: 0, height: 5.0)
-        backButton?.layer.shadowOpacity = 0.1
+        backButton?.layer.shadowOpacity = 0.2
         backButton?.layer.shadowRadius = 8.0
         backButton?.layer.masksToBounds = false
         
@@ -47,7 +85,20 @@ class LoginViewController: UIViewController {
         googleButtton?.layer.shadowRadius = 8.0
         googleButtton?.layer.masksToBounds = false
         
+        emailTextfield?.layer.shadowColor = UIColor.lightGray.cgColor
+        emailTextfield?.layer.shadowOffset = CGSize(width: 0, height: 5.0)
+        emailTextfield?.layer.shadowOpacity = 0.1
+        emailTextfield?.layer.shadowRadius = 8.0
+        emailTextfield?.layer.masksToBounds = false
+        
+        passwordTextfield?.layer.shadowColor = UIColor.lightGray.cgColor
+        passwordTextfield?.layer.shadowOffset = CGSize(width: 0, height: 5.0)
+        passwordTextfield?.layer.shadowOpacity = 0.1
+        passwordTextfield?.layer.shadowRadius = 8.0
+        passwordTextfield?.layer.masksToBounds = false
+        
         
 
     }
 }
+
--- Food Hub/ViewControllers/SignUpViewController.swift
+++ Food Hub/ViewControllers/SignUpViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var googleButton: UIButton!
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var nameTextFieald: UITextField!
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    
+    let customColor = UIColor(hex: 0xFF7252)
+    var selectedTextField: UITextField?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
        skipButtonUI()
-    }
-    
-
-    func skipButtonUI() {
-   
+        passwordTextfield.delegate = self
+        emailTextfield.delegate = self
+        nameTextFieald.delegate = self
         
+        nameTextFieald.tag = 0
+        emailTextfield.tag = 1
+        passwordTextfield.tag = 2
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+                view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        // Boş bir yere tıklandığında klavyeyi kapat
+        view.endEditing(true)
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Seçilen TextField'in sınır rengini değiştir
+        selectedTextField = textField
+        selectedTextField?.layer.borderColor = customColor.cgColor
+        selectedTextField?.layer.borderWidth = 1.0
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Seçilen TextField'in sınır rengini eski haline getir
+        selectedTextField?.layer.borderColor = nil
+        selectedTextField?.layer.borderWidth = 0.0
+        selectedTextField = nil
+    }
+
+        
+    
+    
+    func skipButtonUI() {
         passwordTextfield?.layer.shadowColor = UIColor.lightGray.cgColor
         passwordTextfield?.layer.shadowOffset = CGSize(width: 0, height: 5.0)
         passwordTextfield?.layer.shadowOpacity = 0.1
@@ -60,9 +92,15 @@ class SignUpViewController: UIViewController {
         googleButton?.layer.shadowRadius = 8.0
         googleButton?.layer.masksToBounds = false
         
-    
-        
 
     }
 
+}
+extension UIColor {
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(hex & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
