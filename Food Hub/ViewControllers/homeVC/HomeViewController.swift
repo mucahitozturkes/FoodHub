190ea4eee7f6f3547e5029618bfbba7bbd979f6d
SaGe <m.ozturkes@gmail.com>
2023-12-07 22:07:45 +0300

    home CollectionView section one done

--- Food Hub/ViewControllers/homeVC/HomeViewController.swift
+++ Food Hub/ViewControllers/homeVC/HomeViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-   
+
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet weak var searchTextfield: UITextField!
     @IBOutlet weak var filterButton: UIButton!
     @IBOutlet weak var deliverAdressLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var menuButton: UIButton!
+    
+    var foods = ["Burger", "Donat", "Pizza", "Mexican", "Asian", "Burger", "Donat", "Pizza", "Mexican", "Asian"]
+    var foodsImages = ["Burger", "Donat", "Pizza", "Mexican", "Asian", "Burger", "Donat", "Pizza", "Mexican", "Asian"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,9 +54,6 @@ class HomeViewController: UIViewController {
         searchTextfield?.leftView = searchIconContainerView
         searchTextfield?.leftViewMode = .always
 
-
-
-        
         filterButton?.layer.shadowColor = UIColor.lightGray.cgColor
         filterButton?.layer.shadowOffset = CGSize(width: 0, height: 5.0)
         filterButton?.layer.shadowOpacity = 0.2
@@ -80,9 +83,71 @@ class HomeViewController: UIViewController {
 
 }
 
-class CVCell: UICollectionViewCell {
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+class Cell: UITableViewCell {
+   
+    
+}
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return foods.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CCell", for: indexPath) as! CCell
+        cell.CVlabel.text = foods[indexPath.row]
+
+        if let image = UIImage(named: foods[indexPath.row]) {
+            cell.image.image = image
+        } else {
+            // Handle the case where the image cannot be loaded
+        }
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: collectionView.frame.width, height: 120
+            )
+        }
+}
+class CCell: UICollectionViewCell {
+    @IBOutlet weak var backgroundViewCell: UIView!
     @IBOutlet weak var image: UIImageView!
-    
-    @IBOutlet weak var imageLabel: UILabel!
+    @IBOutlet weak var CVlabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+
+    func configureUI() {
+        // Arkaplan görünümünün köşe yarıçapı ve gölge efekti
+        // Arkaplan görünümünün köşe yarıçapı ve gölge efekti
+               backgroundViewCell.layer.cornerRadius = 30.0
+               backgroundViewCell.layer.masksToBounds = false
+               backgroundViewCell.layer.shadowColor = UIColor.lightGray.cgColor
+               backgroundViewCell.layer.shadowOffset = CGSize(width: 0, height: 0)
+        backgroundViewCell.layer.shadowOpacity = 0.1
+               backgroundViewCell.layer.shadowRadius = 8.0
+               backgroundViewCell.layer.shadowPath = UIBezierPath(roundedRect: backgroundViewCell.bounds, cornerRadius: backgroundViewCell.layer.cornerRadius).cgPath
+
+               // İmajın köşe yarıçapı ve gölge efekti
+        image.layer.cornerRadius = 25.0
+        image.layer.masksToBounds = true
+     
+    }
+
 }
